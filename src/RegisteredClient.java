699e10ed62a1be7d185abd152202e3be2a35ea62
dunap <dunap@LAPTOP-2VAK2E2V>
2026-03-11 17:03:30 +0100

    RegisteredClient, falta desarrollar métodos

--- src/RegisteredClient.java
+++ src/RegisteredClient.java
@@ -2,9 +2,98 @@
  * 
  */
 
-/**
- * 
- */
-public class RegisteredClient {
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
 
+/**
+ * Class name: RegisteredClient
+ * <p>
+ * Description: It implements the registered client
+ * @author Duna P.R.
+ * @version 1.0
+ * @see User
+ */
+public class RegisteredClient extends User {
+	private LocalDate registerDate;
+	private String dni;
+	
+	/**
+	 * Creates a new RegisteredClient
+	 * 
+	 * @param userName the user's name
+	 * @param registerDate the registration date 
+	 * @param dni the user's dni
+	 * @param password the user's password
+	 */
+	public RegisteredClient(String userName, LocalDate registerDate, String dni, String password) {
+		super(userName, password);
+		this.registerDate = registerDate;
+		this.dni = dni;
+	}
+	
+	/**
+	 * Creates a new RegisteredCliente with a set registration date(today)
+	 * 
+	 * @param userName the user's name
+	 * @param dni the user's dni
+	 * @param password the user's password
+	 */
+	public RegisteredClient(String userName, String dni, String password) {
+		this(userName, LocalDate.now(), dni, password);
+	}
+
+	/**
+	 * Obtains the client's registration date
+	 * 
+	 * @return the registerDate the registration date
+	 */
+	public LocalDate getRegisterDate() {
+		return registerDate;
+	}
+
+	/**
+	 * It sets the registration date 
+	 * 
+	 * @param registerDate the registerDate to set
+	 */
+	public void setRegisterDate(LocalDate registerDate) {
+		this.registerDate = registerDate;
+	}
+
+	/**
+	 * Obtains the client's dni
+	 * 
+	 * @return the dni the client's dni
+	 */
+	public String getDni() {
+		return dni;
+	}
+
+	/**
+	 * It sets the dni
+	 * 
+	 * @param dni the dni to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	/**
+	 * It changes the client's password
+	 * 
+	 * @param psswd the new password
+	 */
+	public void changePassword(String psswd) {
+		//Tengo que hacer la comprobación de que la contraseña es segura
+		super.changePassword(psswd);
+	}
+	
+	//public boolean buy() {}
+	//removeFromWallet(Product)
+	//requestValoration
+	//makeAnOffer
+	//browseNotifications
+	
+	
 }

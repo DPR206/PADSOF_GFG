45d6168c7475d787190e7e8a702f90b3b49cdb1e
BlueScreenOfDeath314 <sofia.calvo@estudiante.uam.es>
2026-03-13 18:52:17 +0100

    Implement user login and sign-in functionality
    Added user management methods for login and sign-in.

--- src/store/Store.java
+++ src/store/Store.java
@@ -1,6 +1,8 @@
 package store;
 
+import java.util.*;
 import product.Category;
+import user.*;
 
 // DUE: Falta el @see
 
@@ -12,8 +14,10 @@ import product.Category;
  * @version 1.0
  */
 public class Store {
+	private static final Store INSTANCE;
     private static int productId = 0;
     // Está creada para que no se queje el compilador en sus referencias
+    private Map<String, User> users = new HashMap<>();
 
     public Category getCategoryFromName(String name) {
         return null; // Sustituir y rellenar
@@ -26,5 +30,38 @@ public class Store {
     public int getProductId() {
         int id = this.productId;
         return id;
+    }
+    
+    private boolean logIn(String userName, String pwd) {
+    	User u;
+    	
+    	if(this.users.containsKey(userName)) {
+    		u = this.users.get(userName);
+    		if(u.getPassword() == pwd) {
+    			return true;
+    		}
+    	}
+    	return false;
+    } 
+    
+    public boolean signIn() {
+		Scanner sc = new Scanner(System.in);
+		String userName, pwd;
+		RegisteredClient rc;
+		
+		System.out.print("Introduce tu usuario: ");
+		
+		try {
+			System.out.print("Introduce tu usuario: ");
+			userName = sc.next();
+			System.out.print("Introduce tu contraseña: ");
+			pwd = sc.next();
+		}catch (InputMismatchException e) {
+            System.out.println("Error: El tipo de dato introducido no es válido.");
+        } finally {
+            sc.close();
+        }
+		
+		
     }
 }

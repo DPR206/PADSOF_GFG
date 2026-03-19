9f5b0fa02dd222c84ec343fcdade10786936e57e
Ana O.R. <dTvshow666@gmail.com>
2026-03-19 15:06:27 +0100

    Dejo la idea del test porque aún es muy pronto para hacerlo

--- src/ExchangeDemo.java
+++ src/ExchangeDemo.java
@@ -1,7 +1,43 @@
-import store.Store;
-import user.Manager;
+import store.*;
+import exchange.*;
+import product.*;
+import user.*;
+
+// NOTE: Técnicamente, usaría store como fachada / base de datos
+// NOTE: Es sin buscador ni notificaciones por ahora
 
 public class ExchangeDemo {
     Store s = new Store();
+
+    /* s.addManager */
     Manager manager = new Manager("password", "manager", 0, s);
+
+    /* s.addRegisteredClient */
+    RegisteredClient rc1 = new RegisteredClient("client1", "12345678A", "password");
+    RegisteredClient rc2 = new RegisteredClient("client2", "12345678A", "password");
+
+    /* s.addEmployee <- con exchangePermission */
+    Employee emp = new Employee("password", "exchangeEmployee", 1, Permission.EXCHANGE);
+
+    /*----------------------------------------------- Inicio simulación ----------------------------------------------*/
+
+    /* Cliente 1 sube un producto a su cartera */
+
+    /* Cliente 2 sube un producto a su cartera */
+
+    /* Cliente 1 "busca" productos de segunda mano y hace una oferta a cliente 2 */
+
+    /* Cliente 2 la rechaza */
+
+    /* Cliente 1 "busca" productos de segunda mano y hace una oferta a cliente 2 */
+
+    /* Se pasa el tiempo de la oferta y caduca */
+
+    /* Cliente 1 "busca" productos de segunda mano y hace una oferta a cliente 2 */
+
+    /* Cliente 2 acepta la oferta -> nuevo intercambio disponible */
+
+    /* Empleado de Intercambios lo gestiona (lo marca como intercambiado) */
+
+    /*------------------------------------------------------ Fin -----------------------------------------------------*/
 }

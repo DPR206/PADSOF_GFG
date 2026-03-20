cdd14f05a821c5cfb8b9f6125cf26c964adcf781
dunap <dunap@LAPTOP-2VAK2E2V>
2026-03-20 14:01:48 +0100

    Parameter con metodos basicos

--- src/store/Parameter.java
+++ src/store/Parameter.java
@@ -29,7 +29,56 @@ public class Parameter {
 		OrderTime = Period.ofDays(7);
 		this.valuationCost = 10;
 	}
-	
-	
 
+	/**
+	 * @return the offerTime
+	 */
+	public Period getOfferTime() {
+		return OfferTime;
+	}
+
+	/**
+	 * @param offerTime the offerTime to set
+	 */
+	public void setOfferTime(Period offerTime) {
+		OfferTime = offerTime;
+	}
+
+	/**
+	 * @return the orderTime
+	 */
+	public Period getOrderTime() {
+		return OrderTime;
+	}
+
+	/**
+	 * @param orderTime the orderTime to set
+	 */
+	public void setOrderTime(Period orderTime) {
+		OrderTime = orderTime;
+	}
+
+	/**
+	 * @return the valuationCost
+	 */
+	public double getValuationCost() {
+		return valuationCost;
+	}
+
+	/**
+	 * @param valuationCost the valuationCost to set
+	 */
+	public void setValuationCost(double valuationCost) {
+		this.valuationCost = valuationCost;
+	}
+
+	/**
+	 * @return the param
+	 */
+	public static Parameter getParam() {
+		return PARAM;
+	}
+	
+	
+	
 }

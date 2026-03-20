71331b5312106ad005593f3a9f9906c5a8afe345
Ana O.R. <dTvshow666@gmail.com>
2026-03-20 22:57:29 +0100

    Carpeta doc fuera de src

--- src/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exchange"},{"l":"order"},{"l":"product"},{"l":"store"},{"l":"user"},{"l":"utilities"}];updateSearchResults();

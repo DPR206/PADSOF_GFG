990ee83b513dc2d97eff37cababbebbf4694ac99
Ana O.R. <dTvshow666@gmail.com>
2026-03-27 18:01:28 +0100

    Commit Message

---
+++ test/product/SecondHandProductTest.java
@@ -0,0 +1,104 @@
+package product;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecondHandProductTest {
+
+    @Test
+    void changePrice() {
+    }
+
+    @Test
+    void changeName() {
+    }
+
+    @Test
+    void changeDescription() {
+    }
+
+    @Test
+    void changePhoto() {
+    }
+
+    @Test
+    void changeType() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getPhoto() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getValuationDate() {
+    }
+
+    @Test
+    void setValuationDate() {
+    }
+
+    @Test
+    void isAvailable() {
+    }
+
+    @Test
+    void setAvailable() {
+    }
+
+    @Test
+    void isPaidValuation() {
+    }
+
+    @Test
+    void setPaidValuation() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void changeEstimatedPrice() {
+    }
+
+    @Test
+    void changeConservationStatus() {
+    }
+
+    @Test
+    void changeAvailability() {
+    }
+
+    @Test
+    void valuationWasPaid() {
+    }
+
+    @Test
+    void valuate() {
+    }
+}

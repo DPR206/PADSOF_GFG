77122009d19528f780a7c09d38abb7ee8b0df858
dunap <dunap@192.168.1.105>
2026-03-30 12:25:01 +0200

    Notification order empezado

--- src/notification/NotificationOrder.java
+++ src/notification/NotificationOrder.java
@@ -1,5 +1,56 @@
 package notification;
 
-public class NotificationOrder {
+import java.time.LocalDateTime;
+
+import order.*;
+
+public class NotificationOrder extends Notification implements NotificationInterface<Order>{
+
+	/**
+	 * @param timeReceived
+	 * @param read
+	 * @param visible
+	 */
+	public NotificationOrder(LocalDateTime timeReceived, boolean read, boolean visible) {
+		super(timeReceived, read, visible);
+	}
+
+	/**
+	 * @param title
+	 * @param text
+	 * @param timeReceived
+	 * @param read
+	 * @param visible
+	 */
+	public NotificationOrder(String title, String text, LocalDateTime timeReceived, boolean read, boolean visible) {
+		super(title, text, timeReceived, read, visible);
+	}
+
+	/**
+	 * @param title
+	 * @param text
+	 * @param timeMade
+	 * @param timeReceived
+	 * @param read
+	 * @param visible
+	 */
+	public NotificationOrder(String title, String text, LocalDateTime timeMade, LocalDateTime timeReceived,
+			boolean read, boolean visible) {
+		super(title, text, timeMade, timeReceived, read, visible);
+	}
+
+	@Override
+	public String FullNotification(Order o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String SnippetNotification(Order o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }

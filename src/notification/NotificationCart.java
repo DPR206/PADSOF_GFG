2c49149461f73da7410908117fb9dba55bc3ccf9
dunap <dunap@192.168.1.105>
2026-03-30 13:11:05 +0200

    NotificationCart en desarrollo

--- src/notification/NotificationCart.java
+++ src/notification/NotificationCart.java
@@ -3,9 +3,57 @@
  */
 package notification;
 
+import java.time.LocalDateTime;
+
+import order.*;
+
 /**
  * 
  */
-public class NotificationCart {
+public class NotificationCart extends Notification implements NotificationInterface<Cart>{
 
+	/**
+	 * @param timeReceived
+	 * @param read
+	 * @param visible
+	 */
+	public NotificationCart(LocalDateTime timeReceived, boolean read, boolean visible) {
+		super(timeReceived, read, visible);
+	}
+
+	/**
+	 * @param title
+	 * @param text
+	 * @param timeReceived
+	 * @param read
+	 * @param visible
+	 */
+	public NotificationCart(String title, String text, LocalDateTime timeReceived, boolean read, boolean visible) {
+		super(title, text, timeReceived, read, visible);
+	}
+
+	/**
+	 * @param title
+	 * @param text
+	 * @param timeMade
+	 * @param timeReceived
+	 * @param read
+	 * @param visible
+	 */
+	public NotificationCart(String title, String text, LocalDateTime timeMade, LocalDateTime timeReceived, boolean read,
+			boolean visible) {
+		super(title, text, timeMade, timeReceived, read, visible);
+	}
+
+	@Override
+	public String FullNotification(Cart o) {
+		return null;
+	}
+
+	@Override
+	public String SnippetNotification(Cart o) {
+		return null;
+	}
+
+	
 }

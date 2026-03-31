f34c74563b0382bc0e5bf5e7a2923f492613b1b1
dunap <dunap@192.168.1.105>
2026-03-31 18:13:30 +0200

    Javadoc acabado

--- src/notification/NotificationPayment.java
+++ src/notification/NotificationPayment.java
@@ -2,35 +2,49 @@ package notification;
 
 import java.time.LocalDateTime;
 
+/**
+ * It implements the payment notification
+ * 
+ * @author Duna P.R.
+ * @version 1.2
+ * @see Notification
+ * @see NotificationInterface
+ */
 public class NotificationPayment extends Notification implements NotificationInterface<String>{
 
 	/**
-	 * @param timeReceived
-	 * @param read
-	 * @param visible
+	 * Creates a new payment notification with default timeMade (this moment) and no content
+	 * 
+	 * @param timeReceived the time it was received
+	 * @param read whether the user has read it or not
+	 * @param visible if the notification has been erased by the user (thus no longer visible for them)
 	 */
 	public NotificationPayment(LocalDateTime timeReceived, boolean read, boolean visible) {
 		super(timeReceived, read, visible);
 	}
 
 	/**
-	 * @param title
-	 * @param text
-	 * @param timeReceived
-	 * @param read
-	 * @param visible
+	 * Creates a new payment notification with default timeMade (this moment)
+	 * 
+	 * @param title the tile of the notification
+	 * @param text the message in the notification
+	 * @param timeReceived the time it was received
+	 * @param read whether the user has read it or not
+	 * @param visible if the notification has been erased by the user (thus no longer visible for them)
 	 */
 	public NotificationPayment(String title, String text, LocalDateTime timeReceived, boolean read, boolean visible) {
 		super(title, text, timeReceived, read, visible);
 	}
 
 	/**
-	 * @param title
-	 * @param text
-	 * @param timeMade
-	 * @param timeReceived
-	 * @param read
-	 * @param visible
+	 * Creates a new payment notification
+	 * 
+	 * @param title the tile of the notification
+	 * @param text the message in the notification
+	 * @param timeMade the time it was made
+	 * @param timeReceived the time it was received
+	 * @param read whether the user has read it or not
+	 * @param visible if the notification has been erased by the user (thus no longer visible for them)
 	 */
 	public NotificationPayment(String title, String text, LocalDateTime timeMade, LocalDateTime timeReceived,
 			boolean read, boolean visible) {
@@ -38,6 +52,12 @@ public class NotificationPayment extends Notification implements NotificationInt
 	}
 
 	@Override
+	/**
+	 * Creates a complete notification
+	 * 
+	 * @param o the new discount
+	 * @return a string with the notification
+	 */
 	public String FullNotification(String o) {
 		String text = "Your payment for " + o + " was completed successfully. Once your purchase is ready you'll receive a new notification\n";
 		this.setText(text);
@@ -45,6 +65,12 @@ public class NotificationPayment extends Notification implements NotificationInt
 	}
 
 	@Override
+	/**
+	 * Creates a snippet of the notification (title and time log)
+	 * 
+	 * @param o the new discount
+	 * @return a string with the snippet of the notification
+	 */
 	public String SnippetNotification(String o) {
 		this.setTitle("Your payment was succesful\n");
 		return this.getTitle() + this.timeLog();

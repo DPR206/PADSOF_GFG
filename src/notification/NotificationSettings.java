481bb6894b8e7870bddfbe2e3f62494b30ea3a16
dunap <dunap@192.168.1.105>
2026-03-31 19:36:41 +0200

    Settings (casi está)

--- src/notification/NotificationSettings.java
+++ src/notification/NotificationSettings.java
@@ -22,10 +22,31 @@ public class NotificationSettings {
 	}
 
 	/**
-	 * 
+	 * Creates a new setting
 	 */
 	public NotificationSettings() {
-		this.interests = new HashMap();
+		this.interests = new HashMap<NotificationType, Boolean>(Map.of(
+				NotificationType.EXCHANGE, true,
+			    NotificationType.DISCOUNT, true,
+			    NotificationType.CART, true,
+			    NotificationType.NEW_SECONDHAND_PRODUCT, true,
+			    NotificationType.PAYMENT, true,
+			    NotificationType.ORDER, true
+		));
+	}
+
+	/**
+	 * @return the interests
+	 */
+	public HashMap<NotificationType, Boolean> getInterests() {
+		return interests;
+	}
+
+	/**
+	 * @param interests the interests to set
+	 */
+	public void setInterests(HashMap<NotificationType, Boolean> interests) {
+		this.interests = interests;
 	}
 	
 	

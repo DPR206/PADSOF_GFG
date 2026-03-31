35a00312e8d440762dc0d3b84942482ac2536d44
dunap <dunap@192.168.1.105>
2026-03-31 11:56:09 +0200

    Javadoc

--- src/notification/NotificationCart.java
+++ src/notification/NotificationCart.java
@@ -9,37 +9,45 @@ import order.*;
 import store.Parameter;
 
 /**
- * 
+ * It implements the cart notification
+ * @author Duna P.R.
+ * @version 1.2
  */
 public class NotificationCart extends Notification implements NotificationInterface<Cart>{
 
 	/**
-	 * @param timeReceived
-	 * @param read
-	 * @param visible
+	 * Creates a new cart notification with default timeMade (this moment) and no content
+	 * 
+	 * @param timeReceived the time it was received
+	 * @param read whether the user has read it or not
+	 * @param visible if the notification has been erased by the user (thus no longer visible for them)
 	 */
 	public NotificationCart(LocalDateTime timeReceived, boolean read, boolean visible) {
 		super(timeReceived, read, visible);
 	}
 
 	/**
-	 * @param title
-	 * @param text
-	 * @param timeReceived
-	 * @param read
-	 * @param visible
+	 * Creates a new cart notification with default timeMade (this moment)
+	 * 
+	 * @param title the tile of the notification
+	 * @param text the message in the notification
+	 * @param timeReceived the time it was received
+	 * @param read whether the user has read it or not
+	 * @param visible if the notification has been erased by the user (thus no longer visible for them)
 	 */
 	public NotificationCart(String title, String text, LocalDateTime timeReceived, boolean read, boolean visible) {
 		super(title, text, timeReceived, read, visible);
 	}
 
 	/**
-	 * @param title
-	 * @param text
-	 * @param timeMade
-	 * @param timeReceived
-	 * @param read
-	 * @param visible
+	 * Creates a new cart notification
+	 * 
+	 * @param title the tile of the notification
+	 * @param text the message in the notification
+	 * @param timeMade the time it was made
+	 * @param timeReceived the time it was received
+	 * @param read whether the user has read it or not
+	 * @param visible if the notification has been erased by the user (thus no longer visible for them)
 	 */
 	public NotificationCart(String title, String text, LocalDateTime timeMade, LocalDateTime timeReceived, boolean read,
 			boolean visible) {
@@ -47,6 +55,11 @@ public class NotificationCart extends Notification implements NotificationInterf
 	}
 
 	@Override
+	/**
+	 * Creates a complete notification
+	 * 
+	 * @return a string with the notification
+	 */
 	public String FullNotification(Cart o) {
 		String text = "The products/packs in your cart have expired after " + Parameter.getParam().getOrderTime() 
 						+ " days, and they have been removed.\n";
@@ -55,6 +68,11 @@ public class NotificationCart extends Notification implements NotificationInterf
 	}
 
 	@Override
+	/**
+	 * Creates a snippet of the notification (title and time log)
+	 * 
+	 * @return a string with the snippet of the notification
+	 */
 	public String SnippetNotification(Cart o) {
 		String title = "Your cart has expired\n";
 		this.setTitle(title);

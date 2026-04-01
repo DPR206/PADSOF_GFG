6be9504d9486a619d019d4f5f28cff15387c6435
BlueScreenOfDeath314 <sofia.calvo@estudiante.uam.es>
2026-04-01 13:21:42 +0200

    actualización del searcher

--- src/search/Searcher.java
+++ src/search/Searcher.java
@@ -19,43 +19,51 @@ public class Searcher{
     private Searcher<Employee> employeeSearcher = new Searcher<>(new SearchEmployee());
     private Searcher<Order> orderSearcher = new Searcher<>(new SearchOrder());
     private Searcher<Exchange> exchangeSearcher = new Searcher<>(new SearchExchange());
+
+    private BrowseSecondHandProducts secondHandProducts;
     
-    private Searcher(SearchStoreProducts searchStore, SearchType... types){
+    public Searcher(SearchStoreProducts searchStore, SearchType... types){
         this.searchStore = searchStore;
         this.types = types;
     }
 
-    private List<StoreProduct> searchStoreProducts(){
+    public List<StoreProduct> searchStoreProducts(){
         return this.searchStore.searchStoreProducts();
     }
-    private List<StoreProduct> searchByCategory(Category... c){
+    public List<StoreProduct> searchByCategory(Category... c){
         
         return this.searchStore.searchStoreProducts();
     }
-    private Pack searchPackByID(int id){
+    public Pack searchPackByID(int id){
         if(this.linearSearch(SearchType.S_PACK)){
             return this.packSearcher.searchByID();
         }
         return null;
     }
-    private Exchange searchExchangeByID(int id){
+    public Exchange searchExchangeByID(int id){
         if(this.linearSearch(SearchType.S_EXCHANGE)){
             return this.exchangeSearcher.searchByID();
         }
         return null;
     }
-    private Order searchOrderByID(int id){
+    public Order searchOrderByID(int id){
         if(this.linearSearch(SearchType.S_ORDER)){
             return this.orderSearcher.searchByID();
         }
         return null;
     }
-    private Order searchEmployeeByID(int id){
+    public Order searchEmployeeByID(int id){
         if(this.linearSearch(SearchType.S_EMPLOYEE)){
             return this.employeeSearcher.searchByID();
         }
         return null;
     }
+    public List<SecondHandProduct> browseSecondHandProduct(){
+        if(this.linearSearch(SearchType.S_SECOND_HAND)){
+            return this.BrowseSecondHandProducts.searchSecondHandProducts();
+        }
+        return null;
+    } 
 
     private boolean linearSearch(SearchType t){
         for(SearchType type: this.types){

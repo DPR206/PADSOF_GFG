27b05aae809bb841433dc14320eb30ebb37bbeb3
dunap <dunap@192.168.1.105>
2026-04-01 19:53:32 +0200

    El principio del fin

--- src/store/Statistics.java
+++ src/store/Statistics.java
@@ -3,6 +3,12 @@
  */
 package store;
 
+import java.util.*;
+import java.time.*;
+
+import user.RegisteredClient;
+import product.*;
+
 /**
  * It implements the statistics
  * 
@@ -10,5 +16,11 @@ package store;
  * @version 1.1
  */
 public class Statistics {
-
+	private static Statistics INSTANCE = null;
+	
+	private HashMap<Month, Double> revenueByMonth;
+	private RegisteredClient clients;
+	private Category categories;
+	private double revenue_valuation;
+	
 }

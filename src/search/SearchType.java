612595ce4f87c82cb6b1099fd21e2a4ecc67db12
BlueScreenOfDeath314 <sofia.calvo@estudiante.uam.es>
2026-04-02 10:35:07 +0200

    documentación realizada

--- src/search/SearchType.java
+++ src/search/SearchType.java
@@ -1,5 +1,13 @@
 package search;
 
+/**
+    * Enum name: SearchType
+    * <p>
+    * Description: It implements the type of searches that will be done
+    * @author Sofía C.L.
+    * @version 1.3
+    * @see Search
+    */
 public enum SearchType{
     S_STORE("store"),
     S_ORDER("order"),
@@ -11,9 +19,20 @@ public enum SearchType{
 
     private String theType;
 
+    /**
+	 * Creates the enum and initiates the atribute of the name of the type of search
+	 *
+	 * @param asc, determines if the searches are ascendant or descendant
+     * @param type, determines the type of search we are doing
+	 */
     private SearchType(String theType){
         this.theType = theType;
     }
+
+    /**
+	 * Gets the the String of the type
+	 *
+	 */
     public String getType(){
         return this.theType;
     }
--- src/search/Searcher.java
+++ src/search/Searcher.java
@@ -22,42 +22,87 @@ public class Searcher{
 
     private BrowseSecondHandProducts secondHandProducts;
     
+    /**
+	 * Creates the master class searcher
+	 *
+	 * @param searchStore, the class to search products
+     * @param types, array of the types of searching that will be done
+	 */
     public Searcher(SearchStoreProducts searchStore, SearchType... types){
         this.searchStore = searchStore;
         this.types = types;
     }
 
+     /**
+	 * Searches store products depending on the filters implemented in the SearchStoreProducts class
+	 *
+	 */
     public List<StoreProduct> searchStoreProducts(){
         return this.searchStore.searchStoreProducts();
     }
+
+    /**
+	 * Searches store products depending on the categories
+     * 
+	 *@param c, the array of the categories of the products we want to search
+	 */
     public List<StoreProduct> searchByCategory(Category... c){
         
         return this.searchStore.searchStoreProducts();
     }
+
+    /**
+	 * Searches the pack based on the id
+	 *
+     * @param id, searches the pack by the id
+	 */
     public Pack searchPackByID(int id){
         if(this.linearSearch(SearchType.S_PACK)){
             return this.packSearcher.searchByID();
         }
         return null;
     }
+
+    /**
+	 * Searches the exchange based on the id
+	 *
+     * @param id, searches the exchange by the id
+	 */
     public Exchange searchExchangeByID(int id){
         if(this.linearSearch(SearchType.S_EXCHANGE)){
             return this.exchangeSearcher.searchByID();
         }
         return null;
     }
+
+    /**
+	 * Searches the order based on the id
+	 *
+     * @param id, searches the order by the id
+	 */
     public Order searchOrderByID(int id){
         if(this.linearSearch(SearchType.S_ORDER)){
             return this.orderSearcher.searchByID();
         }
         return null;
     }
+
+    /**
+	 * Searches the employee based on the id
+	 *
+     * @param id, searches the employee by the id
+	 */
     public Order searchEmployeeByID(int id){
         if(this.linearSearch(SearchType.S_EMPLOYEE)){
             return this.employeeSearcher.searchByID();
         }
         return null;
     }
+
+    /**
+	 * Browses all the second hand products ordered in alphabetical order
+	 *
+	 */
     public List<SecondHandProduct> browseSecondHandProduct(){
         if(this.linearSearch(SearchType.S_SECOND_HAND)){
             return this.BrowseSecondHandProducts.searchSecondHandProducts();
@@ -65,6 +110,11 @@ public class Searcher{
         return null;
     } 
 
+    /**
+	 * Linear search implemented to search for the searchtype on the array of searchTypes
+     * 
+	 *@param t, the searchType we want to check if it's there or not
+	 */
     private boolean linearSearch(SearchType t){
         for(SearchType type: this.types){
             if(t.getType().equals(type.getType())) return true

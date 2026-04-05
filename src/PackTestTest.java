9b94dbe189fc9f1694e485b3cc50a6b796ffddc4
BlueScreenOfDeath314 <sofia.calvo@estudiante.uam.es>
2026-04-05 16:27:40 +0200

    problemas con tests xd

---
+++ src/PackTestTest.java
@@ -0,0 +1,97 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PackTestTest {
+
+	@Test
+	public void testGetDiscount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDiscount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrintInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProducts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProducts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDateAddCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDateAddCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecreaseStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncreaseStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminateProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddArrayProducts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminateArrayProducts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTotalPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrintProducts() {
+		fail("Not yet implemented");
+	}
+
+}

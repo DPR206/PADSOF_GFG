ef8f9ab59430e0d541ed672b9c342e107fd442dd
dunap <dunap@LAPTOP-2VAK2E2V>
2026-04-06 22:05:21 +0200

    Mejora

--- test/user/ExchangePermissionTest.java
+++ test/user/ExchangePermissionTest.java
@@ -1,8 +1,8 @@
 package user;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class ExchangePermissionTest {
 

f532a7f92dabe8ba3aa2966af4e2f4bd8616d816
Ana O.R. <dTvshow666@gmail.com>
2026-04-06 23:29:27 +0200

    Resulta que cart es mucho más interesante de lo que pensaba... cuando más trabajo más trabajo me sale

--- src/main/UnregisteredClientLoop.java
+++ src/main/UnregisteredClientLoop.java
@@ -1,6 +1,7 @@
 package main;
 
 import order.Cart;
+import product.Category;
 import product.StoreProduct;
 import store.Pager;
 import store.Store;
@@ -8,6 +9,7 @@ import user.RegisteredClient;
 import user.UnregisteredClient;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -58,7 +60,7 @@ public class UnregisteredClientLoop extends Loop {
         switch (chosenOption) {
             case 1: /* Browse products */
                 enterPagedScreen();
-                browseStore();
+                browseStore(new ArrayList<>());
                 break;
             case 2: /* Log in */
                 logger();
@@ -85,16 +87,19 @@ public class UnregisteredClientLoop extends Loop {
      * @throws IllegalArgumentException the illegal argument exception
      * @throws NullPointerException     the null pointer exception
      */
-    private void browseStore() throws IOException, IllegalArgumentException, NullPointerException {
+    private void browseStore(List<StoreProduct> filteredStore)
+            throws IOException, IllegalArgumentException, NullPointerException {
         System.out.print("\n ---- browseStore ---- \n"); // Es para debug, borrar
         System.out.println("Page: " + currentScreenPageNum);
 
-        List<StoreProduct> products = ((UnregisteredClient) currentUser).searchStoreProduct();
-        Pager.getInstance().printStoreProductListPage(products, currentScreenPageNum);
+        if (filteredStore.isEmpty()) { // Si no hay filtros se muestran todos los productos
+            filteredStore = ((UnregisteredClient) currentUser).searchStoreProduct();
+        }
+        Pager.getInstance().printStoreProductListPage(filteredStore, currentScreenPageNum);
 
         System.out.println("What do you wish to do? (enter the nº)");
         int i = 1;
-        System.out.println("\t[" + i++ + "] Filter search");
+        System.out.println("\t[" + i++ + "] Add a filter to the search");
         System.out.println("\t[" + i++ + "] See a product");
         System.out.println("\t[" + i++ + "] See my cart");
         System.out.println("\t[" + i++ + "] Sign up");
@@ -103,7 +108,9 @@ public class UnregisteredClientLoop extends Loop {
 
         switch (chosenOption) {
             case 1: /* Filter search */
-                System.out.println("Not yet implemented"); // DUE: Filter search
+                filteredStore = filterSearch();
+                System.out.println("Applying filters...");
+                browseStore(filteredStore);
                 break;
             case 2: /* See a product */
                 System.out.print("\n ---- seeStoreProduct ---- \n"); // Es para debug, borrar
@@ -122,11 +129,11 @@ public class UnregisteredClientLoop extends Loop {
                 break;
             case 5: /* Previous page */
                 previousPage();
-                browseStore();
+                browseStore(filteredStore);
                 break;
             case 6: /* Next page */
-                nextPageStoreProduct(products);
-                browseStore();
+                nextPageStoreProduct(filteredStore);
+                browseStore(filteredStore);
                 break;
             case 7: /* Go back */
                 leavePagedScreen();
@@ -142,9 +149,74 @@ public class UnregisteredClientLoop extends Loop {
                 break;
             default:
                 System.out.println("Uh oh, something went wrong :/, reloading...");
-                browseStore();
+                browseStore(filteredStore);
                 break;
         }
+    }
+
+    /**
+     * It allows the unregistered client to filter the store's store products
+     * @return the filtered list of store products
+     */
+    public List<StoreProduct> filterSearch() throws IOException {
+        System.out.println("Choose which filter you wish to apply next (those already applied will be reset)");
+        int i = 1;
+        List<Category> categories = new ArrayList<>();
+        System.out.println("\t[" + i++ + "] Categories");
+        System.out.println("\t[" + i++ + "] Price");
+        System.out.println("\t[" + i++ + "] Review Score");
+        System.out.println("\t[" + i++ + "] Sign up");
+        basicLoopPrinter(i);
+        chosenOption = scanner.nextInt();
+
+        boolean stop = false;
+        switch (chosenOption) {
+            case 1: /* Filter by categories */
+                while (!stop) {
+                    System.out.println("Enter the desired category (type \"stop\" to apply the ones entered so far):");
+                    String categoryName = scanner.next();
+                    if (categoryName.equals("stop")) {
+                        stop = true;
+                    }
+                    Category category = Store.getInstance().getCategoryFromName(categoryName);
+                    if (category == null) {
+                        System.out.println("A category which such a name doesn't exist, reloading...");
+                        return filterSearch();
+                    }
+                    categories.add(category);
+                }
+                break;
+            case 2: /* Filter by price */
+                System.out.println("Enter the minimum desired price:");
+                int minPrice = scanner.nextInt();
+                System.out.println("Enter the maximum desired price:");
+                int maxPrice = scanner.nextInt();
+                if (maxPrice < minPrice) {
+                    System.out.println("Maximum price must be greater or equal to minimum price, reloading...");
+                    return filterSearch();
+                }
+                currentUser.addPriceFilter(minPrice, maxPrice);
+                break;
+            case 3: /* Filter by review score */
+                System.out.println("Enter the minimum desired review score:");
+                int minScore = scanner.nextInt();
+                System.out.println("Enter the maximum desired review score:");
+                int maxScore = scanner.nextInt();
+                if (maxScore < minScore) {
+                    System.out.println("Maximum score must be greater or equal to minimum score, reloading...");
+                    return filterSearch();
+                }
+                currentUser.addPunctuationFilter(minScore, maxScore);
+                break;
+            case 4: /* Sign up */
+                signer();
+                break;
+            default:
+                System.out.println("Uh oh, something went wrong :/, reloading...");
+                return filterSearch();
+        }
+
+        return ((UnregisteredClient) currentUser).searchStoreProductByCategory(categories.toArray(new Category[0]));
     }
 
     /**
@@ -226,11 +298,11 @@ public class UnregisteredClientLoop extends Loop {
                     ((UnregisteredClient) currentUser).addCart(product);
                 }
                 System.out.println("Added " + numProds + " copies to your cart");
-                browseStore();
+                browseStore(new ArrayList<>());
                 break;
             case 5: /* Go back */
                 returnToPagedScreen();
-                browseStore();
+                browseStore(new ArrayList<>());
                 break;
             case 6: /* Go to main page */
                 main();
@@ -326,7 +398,7 @@ public class UnregisteredClientLoop extends Loop {
                 break;
             case 5: /* Go back */
                 returnToPagedScreen();
-                browseStore();
+                browseStore(new ArrayList<>());
                 break;
             case 6: /* Go to main page */
                 leavePagedScreen();

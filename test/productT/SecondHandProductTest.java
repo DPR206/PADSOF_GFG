177a393b819dbd1847efde9364a8df7ff0eb1594
dunap <dunap@LAPTOP-2VAK2E2V>
2026-04-08 12:44:26 +0200

    Tests arreglados (pasan todos)

--- test/productT/SecondHandProductTest.java
+++ test/productT/SecondHandProductTest.java
@@ -1,8 +1,8 @@
 package productT;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.time.LocalDate;
 

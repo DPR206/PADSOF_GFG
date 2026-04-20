21eef724371b4a367affb3f3d4d640fff1e66f16
BlueScreenOfDeath314 <sofia.calvo@estudiante.uam.es>
2026-04-20 12:27:37 +0200

    Update HomePanel.java

--- src/graphicInt/HomePanel.java
+++ src/graphicInt/HomePanel.java
@@ -1,8 +1,9 @@
 package graphicInt;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class HomePanel extends JPanel{
 	
@@ -10,6 +11,36 @@ public class HomePanel extends JPanel{
 		super();
 		this.setLayout(new BorderLayout());
 		
-		JPanel title = new JPanel();
+		JPanel titles = new JPanel();
+		titles.setLayout(new GridLayout(1,3));
+		JPanel home = new JPanel();
+		home.add(new JLabel("Home"));
+		titles.add(home);
+		
+		JPanel name = new JPanel();
+		name.add(new JLabel("GIFTS FOR GEEKS"));
+		titles.add(name);
+		
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new GridLayout(1,2));
+		
+		buttons.add(new JButton("Cart"));
+		buttons.add(new JButton("Account"));
+		titles.add(buttons);
+		
+		this.add(titles, BorderLayout.NORTH);
+		
+		/*PRODUCTOS*/
+		JPanel products = new JPanel();
+		products.setLayout(new BorderLayout());
+		/*SearchBar*/
+		JPanel searcher = new JPanel(new GridLayout(1,2));
+		searcher.add(new JTextField());
+		searcher.add(new JButton("BUSCAR"));
+		products.add(searcher, BorderLayout.NORTH);
+		
+		this.add(products, BorderLayout.CENTER);
+		/*Añadimos products a homepanel*/
+		
 	}
 }

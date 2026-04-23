7e345fe2c7352fa9b9f7555cd570018f7ddedb8f
BlueScreenOfDeath314 <sofia.calvo@estudiante.uam.es>
2026-04-23 10:11:14 +0200

    más cosas del searchControler

---
+++ src/controller/SearcherC.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.store.Store;
+import view.App;
+import view.SearchPanel;
+import view.SignupP;
+
+public class SearcherC implements ActionListener{
+	private final SearchPanel view; /* view -> panel */
+    private final App frame; /* view -> frame */
+    private final Store model; /* model */
+    
+    public SearcherC() {
+    	
+    }
+    
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

6bca80559f1c99ab0f9602d6a758f835260869b0
Ana O.R. <dTvshow666@gmail.com>
2026-04-28 09:34:07 +0200

    Mini avances

--- src/model/user/UnregisteredClient.java
+++ src/model/user/UnregisteredClient.java
@@ -3,21 +3,17 @@
  */
 package model.user;
 
-import model.utilities.IdType;
-import model.utilities.exceptions.*;
-import model.utilities.exceptions.*;
+import es.uam.eps.padsof.telecard.*;
 import model.order.Cart;
 import model.product.*;
 import model.search.SearchType;
 import model.store.Store;
+import model.utilities.IdType;
+import model.utilities.exceptions.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
-
-import es.uam.eps.padsof.telecard.FailedInternetConnectionException;
-import es.uam.eps.padsof.telecard.InvalidCardNumberException;
-import es.uam.eps.padsof.telecard.OrderRejectedException;
 
 /**
  * It implements the unregistered client
@@ -32,13 +28,14 @@ public class UnregisteredClient extends User implements Serializable {
     private Cart c;
     private Store s;
 
-   /**
-    * Creates a new unregistered client
-    * @param asc the products' order in the search
-    */
+/*------------------------------------------------- CONSTRUCTOR --------------------------------------------------*/
+    /**
+     * Creates a new unregistered client
+     * @param asc the products' order in the search
+     */
     public UnregisteredClient(boolean asc) {
         super(UserType.UNREGISTERED_CLIENT, null, null, asc);
-    	c = new Cart();
+        c = new Cart();
         s = Store.getInstance();
 
         this.getSearcher().setTypes(SearchType.S_STORE);
@@ -50,32 +47,30 @@ public class UnregisteredClient extends User implements Serializable {
     public void buy(String username, String password, String dni) throws UsernameTaken, PasswordNotValid, InvalidDni {
         this.s.signIn(username, password, dni, IdType.DNI); //DUE: Contemplar que se use otro
         try {
-			this.c.payOrder();
-		} catch (InvalidCardNumberException e) {
-			System.out.println("Invalid card number");
-		} catch (FailedInternetConnectionException e) {
-			System.out.println("Failed Internet connection");
-		} catch (OrderRejectedException e) {
-			System.out.println("Order rejected");
-		}
+            this.c.payOrder();
+        } catch (InvalidCardNumberException e) {
+            System.out.println("Invalid card number");
+        } catch (FailedInternetConnectionException e) {
+            System.out.println("Failed Internet connection");
+        } catch (OrderRejectedException e) {
+            System.out.println("Order rejected");
+        }
     }
 
     /**
      * Removes a product from the cart
-     *
      * @param product, the product to remove
      */
-    public void deleteCart(StoreProduct product){
-    	this.c.cancelProduct(product);
+    public void deleteCart(StoreProduct product) {
+        this.c.cancelProduct(product);
     }
 
     /**
      * Removes a pack from the cart
-     *
      * @param pack, the pack to remove
      */
     public void deleteCart(Pack pack) {
-    	this.c.cancelPack(pack);
+        this.c.cancelPack(pack);
     }
 
     /**
@@ -83,43 +78,40 @@ public class UnregisteredClient extends User implements Serializable {
      * @param product, the product to add
      */
     public void addCart(StoreProduct product) {
-    	this.c.addProduct(product);
+        this.c.addProduct(product);
     }
 
     /**
      * Adds a pack to the cart
-     *
      * @param pack, the pack to add
      */
     public void addCart(Pack pack) {
-    	this.c.addPack(pack);
-    }
-
-    /**
-     * It gets the unregistered client's cart
-     * @return the unregistered client's cart
-     */
-    public Cart getCart(){
-        return this.c;
+        this.c.addPack(pack);
     }
 
     /**
      * Searches for the store products
      * @return the store product based on the filters
      */
-    public List<StoreProduct> searchStoreProduct(){
-        return this.getSearcher().searchStoreProducts();
+    public List<StoreProduct> searchStoreProduct() {
+        return Store.getInstance().getStoreProductList();
     }
 
     /**
      * Searches for the store products based on the category
-     *
      * @param c, the categories we want our searched products to belong to
-     *
      * @return the store product based on the filters
      */
-    public List<StoreProduct> searchStoreProductByCategory(Category... c){
+    public List<StoreProduct> searchStoreProductByCategory(Category... c) {
         return this.getSearcher().searchByCategory(c);
+    }
+
+    /**
+     * It gets the unregistered client's cart
+     * @return the unregistered client's cart
+     */
+    public Cart getCart() {
+        return this.c;
     }
 
 }
--- src/view/App.java
+++ src/view/App.java
@@ -2,8 +2,7 @@ package view;
 
 import controller.*;
 import model.store.Store;
-import model.user.UnregisteredClient;
-import model.user.User;
+import model.user.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -60,6 +59,9 @@ public class App extends JFrame {
         loginPanel.setController(loginController);
         signupPanel.setController(signupController);
         unregisteredMainPanel.setController(unregisteredMainController);
+        registeredMainPanel.setController(registeredMainController);
+        employeeMainPanel.setController(employeeMainController);
+        managerMainPanel.setController(managerMainController);
         welcomePanel.setController(welcomeController);
 
         /* Add views to main window */
@@ -100,6 +102,9 @@ public class App extends JFrame {
         this.setSize(700, 500);
         this.setLocationRelativeTo(null);
 
+        Store.getInstance().addUser(
+                new Employee("everythingEmployee", "password", true, Permission.STORE, Permission.ORDER,
+                        Permission.EXCHANGE));
     }
 
     /*----------------------------------------------------- MISC -----------------------------------------------------*/

859061e858f3d8e66eb152f739110d7c1d902898
dunap <dunap@LAPTOP-2VAK2E2V>
2026-05-02 20:35:20 +0200

    Controlador manager banner

---
+++ src/controller/BannerManagerC.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.awt.Window;
+
+import javax.swing.SwingUtilities;
+
+import view.App;
+import view.BannerManager;
+
+public class BannerManagerC {
+
+	private BannerManager vista;
+	//private Manager user;
+	private App frame;
+
+	/**
+	 * @param vista
+	 */
+	public BannerManagerC(BannerManager vista, App frame) {
+		this.vista = vista;
+		//this.user = frame.getUser();
+		this.frame = frame;
+        inicializarEventos();
+	}
+	
+	
+	private void inicializarEventos() {
+        
+        vista.getHome().addActionListener(e -> {
+        	abrirPaginaPrincipal();
+        });
+        
+        vista.getBtnPerfil().addActionListener(e -> {
+        	abrirPerfil();
+        });
+    }
+	
+	private void abrirPerfil() {
+		Window ventanaActual = SwingUtilities.getWindowAncestor(vista);
+	    
+	    if (ventanaActual != null) {
+	        ventanaActual.dispose(); // Cerramos la ventana
+	    }
+		
+	    ManagerPerfil perfil = new ManagerPerfil();
+	    
+	    new ManagerPerfil();
+	    
+	    perfil.setVisible(true);
+	}
+
+
+	private void abrirPaginaPrincipal() {
+		
+		Window ventanaActual = SwingUtilities.getWindowAncestor(vista);
+	    
+	    if (ventanaActual != null) {
+	        ventanaActual.dispose(); // Cerramos la ventana
+	    }
+	    
+	    frame.getManagerMainPanel().setVisible(true);;
+	}
+
+}

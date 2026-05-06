0694c61f5fb976f622d3c2e54d54e8d405c06b51
Ana O.R. <dTvshow666@gmail.com>
2026-05-06 18:54:54 +0200

    Clases vacías para compilar

--- src/controller/BannerUnregisteredC.java
+++ src/controller/BannerUnregisteredC.java
@@ -6,13 +6,11 @@ import javax.swing.SwingUtilities;
 
 import model.store.Store;
 import model.user.*;
-import view.App;
-import view.SignupP;
-import view.UnregisteredMainP;
+import view.*;
 import view.banners.BannerUnregistered;
 
 public class BannerUnregisteredC {
-	
+
 	private BannerUnregistered vista;
 	//private UnregisteredClient user;
 	private App frame;
@@ -26,71 +24,71 @@ public class BannerUnregisteredC {
 		this.frame = frame;
         inicializarEventos();
 	}
-	
-	
+
+
 	private void inicializarEventos() {
         vista.getBtnCarrito().addActionListener(e -> {
             abrirCarritoDelCliente();
         });
-        
+
         vista.getHome().addActionListener(e -> {
         	abrirPaginaPrincipal();
         });
-        
+
         vista.getBtnPerfil().addActionListener(e -> {
         	abrirSignUp();
         });
     }
-	
+
 	private void abrirSignUp() {
 		Window ventanaActual = SwingUtilities.getWindowAncestor(vista);
-	    
+
 	    if (ventanaActual != null) {
 	        ventanaActual.dispose(); // Cerramos la ventana
 	    }
-		
+
 	    //SignupP signUp = frame.getSignupPanel();
-	    
+
 	    //new SignupC(frame, Store.getInstance());
-	    
+
 	    frame.getSignupPanel().setVisible(true);
 	}
 
 
 	private void abrirPaginaPrincipal() {
-		
+
 		Window ventanaActual = SwingUtilities.getWindowAncestor(vista);
-	    
+
 	    if (ventanaActual != null) {
 	        ventanaActual.dispose(); // Cerramos la ventana
 	    }
-	    
+
 	    //UnregisteredMainP pagPrin = frame.getUnregisteredMainPanel();
-	    
+
 	    //new UnregisteredMainC(pagPrin, user);
 	    //new UnregisteredMainC(frame, Store.getInstance());
-	    
+
 	    frame.getUnregisteredMainPanel().setVisible(true);
 	}
 
 
 	private void abrirCarritoDelCliente() {
-		
+
 		Window ventanaActual = SwingUtilities.getWindowAncestor(vista);
-	    
+
 	    if (ventanaActual != null) {
 	        ventanaActual.dispose(); // Cerramos la ventana
 	    }
-        
+
         // 1. Crear la vista del carrito
-        CarritoP carritoVista = new CarritoP(); 
-        
+        CarritoP carritoVista = new CarritoP();
+
         // 2. Crear el controlador del carrito pasando el usuario actual
-        new CarritoC(carritoVista, user);
-        
+        //new CarritoC(carritoVista, user);
+
         // 3. Mostrar la ventana
         carritoVista.setVisible(true);
-        
+
     }
 
 }
